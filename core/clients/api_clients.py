e9a39c3462959060313ddb0cba01760926427e95
PashaTen <mamedoff1337@gmail.com>
2026-02-27 22:58:26 +0300

    Написал методы CreateBooking, DeleteBooking, GetBookingId UpdateBooking PartialUpdateBooking

--- core/clients/api_clients.py
+++ core/clients/api_clients.py
@@ -118,4 +118,3 @@ class ApiClients:
             with allure.step("Checking status code"):
                 assert response.status_code == 200, f"Expected status 200 but got {response.status_code}"
             return response.json()
-
